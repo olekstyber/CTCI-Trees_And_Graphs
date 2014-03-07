bc61f03e23780366cf033869fd4b9d40a1af5f54
oleksandr <otyberke@umich.edu>
2014-03-07 03:56:44 -0500

    moved the binaryTree implementation.

---
+++ binaryTree.cpp
@@ -0,0 +1,109 @@
+#include "binaryTree.h"
+
+using namespace std;
+//This is an implementation of a binary tree data structure.
+
+template<class T>
+BinaryTree<T>::BinaryTree(){
+	root = NULL;
+}
+
+template<class T>
+BinaryTree<T>::BinaryTree(T rootValue){
+	node<T>* myRoot = new node<T>;
+	myRoot->left = NULL;
+	myRoot->right = NULL;
+	myRoot->value = rootValue;
+	this->root = myRoot;
+}
+
+template<class T>
+BinaryTree<T>::~BinaryTree(){
+	destroy(root);
+}
+
+template<class T>
+void BinaryTree<T>::insert(T key){
+	node<T>* newNode = new node<T>;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->value = key;
+	if(root == NULL){
+		root = newNode;
+		return;
+	}
+	insert(newNode, root);
+}
+
+template<class T>
+void BinaryTree<T>::insert(node<T>* inNode, node<T>* currNode){
+	if(inNode->value <= currNode->value){
+		if(currNode->left == NULL){
+			currNode->left = inNode;
+		} else insert(inNode, currNode->left);
+	}
+	else{
+		if(currNode->right == NULL){
+			currNode->right = inNode;
+		} else insert(inNode, currNode->right);
+	}
+}
+
+template<class T>
+node<T>* BinaryTree<T>::search(T key){
+	return search(key, root);
+}
+
+template<class T>
+node<T>* BinaryTree<T>::search(T key, node<T>* currNode){
+	if(currNode == NULL) return NULL;
+	else if(key == currNode->value) return currNode;
+	else if(key < currNode->value) return search(key, currNode->left);
+	else return search(key, currNode->right);
+}
+
+template <class T>
+void BinaryTree<T>::destroy(){
+	destroy(root);
+	root = NULL;
+}
+
+template <class T>
+void BinaryTree<T>::destroy(node<T>* target){
+	if(target != NULL){
+		destroy(target->left);
+		destroy(target->right);
+		cout << "DELETING TARGET " << target->value << endl;
+		delete target;
+	}
+}
+
+template <class T>
+void BinaryTree<T>::print(){
+	print(root);
+	cout << endl;
+}
+
+template <class T>
+void BinaryTree<T>::print(node<T>* n){
+	if(n == NULL){
+		return;
+	}
+	cout << n->value << " ";
+	print(n->left);
+	cout << "  ";
+	print(n->right);
+}
+
+int main(){
+	//char data[4] = {"a", "b", "c", "d"};
+	BinaryTree<char> newTree('a');
+	newTree.insert('z');
+	newTree.insert('d');
+	newTree.insert('r');
+	newTree.insert('f');
+	node<char>* search = newTree.search('d');
+	cout << search->value << endl;
+	newTree.print();
+	return 0;
+}
---
+++ binaryTree.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+template <typename T>
+struct node{
+	node* left;
+	node* right;
+	T value;
+};
+
+template <class T>
+class BinaryTree{
+
+private:
+	node<T>* root;
+	void insert(node<T>* inNode, node<T>* currNode);
+	node<T>* search(T key, node<T>* currNode);
+	void print(node<T>* n);
+	void destroy(node<T>* target);
+
+public:
+	BinaryTree();
+	BinaryTree(T rootValue);
+	~BinaryTree();
+
+	void insert(T key);
+	node<T>* search(T key);
+	void print();
+	void destroy();
+	
+};
+
